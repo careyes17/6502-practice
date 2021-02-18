b2563d06eaf6138a039838418fb582884156afe7
careyes17 <careyes4820@eagle.fgcu.edu>
2021-02-18 15:11:04 -0500

    colors rewrite

--- demos/colors/colors.asm
+++ demos/colors/colors.asm
@@ -1,255 +1,69 @@
-StartOfFrame
 
-
-
-            processor 6502
-
-            include "../../lib/vcs.asm"
-            include "../../lib/macro.asm"
-
-
-
-            SEG
-
-            ORG $F000
-
-
-
-Reset
-
-StartOfFrame
-
-
-
-   ; Start of vertical blank processing
-
-
-
-            lda #0
-
-            sta VBLANK
-
-
-
-            lda #2
-
-            sta VSYNC
-
-            
-
-               ; 3 scanlines of VSYNCH signal...
-
-
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-
-
-            lda #0
-
-            sta VSYNC           
-
-
-
-               ; 37 scanlines of vertical blank...
-
-
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-            
-
-
-
-
-
-               ; 192 scanlines of picture...
-
- 
-
-                ldx #0
-
-                REPEAT 192; scanlines
-
-
-
-                    inx
-
-                    stx COLUBK
-
-                    sta WSYNC
-
-
-
-                REPEND
-
-            ; reseting background color to black
-            lda #0
-            sta COLUBK
-
-            lda #%01000010
-
-            sta VBLANK                     ; end of screen - enter blanking
-
-               ; 30 scanlines of overscan...
-
-
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-                sta WSYNC
-
-
-
-            jmp StartOfFrame
-
-
-
-
-
-            ORG $FFFA
-
-
-
-            .word Reset          ; NMI
-
-            .word Reset          ; RESET
-
-            .word Reset          ; IRQ
-
-
-
-    	END
-
+    processor 6502
+    include "../../lib/vcs.asm"
+    include "../../lib/macro.asm"
+
+    ; rom code start
+    SEG
+    ORG $F000
+
+start
+
+    ; Start of vertical blank processing
+    lda #0
+    sta VBLANK
+
+    lda #2
+    sta VSYNC
+
+    ; 3 scanlines of VSYNC signal
+    sta WSYNC
+    sta WSYNC
+    sta WSYNC
+
+    lda #0
+    sta VSYNC
+
+    ; 37 scanlines of vertical blank...
+    ldx #0
+VerticalBlank
+    sta WSYNC
+    inx
+    cpx #37
+    bne VerticalBlank
+
+    ; 192 scanlines of picture...
+    ldx #192
+scanlines
+    sta WSYNC
+    dex
+    stx COLUBK
+    bne scanlines
+
+    ; reseting background color to black
+    lda #0
+    sta COLUBK
+
+    ; Overscan blanking
+    lda #%01000010
+    sta VBLANK ; end of screen - enter blanking
+
+   ; 30 scanlines of overscan...
+
+    ldx #0
+Overscan        
+    sta WSYNC
+    inx
+    cpx #30
+    bne Overscan
+
+    jmp start
+
+    ;interup vectors
+
+    ORG $FFFA
+InterruptVectors
+    .word start          ; NMI
+    .word start          ; RESET
+    .word start          ; IRQ
+END
