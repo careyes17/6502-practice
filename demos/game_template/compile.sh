3868ddc87ba7a0abbc99436a4f544e61d2aed31c
careyes17 <careyes4820@eagle.fgcu.edu>
2021-02-18 23:54:19 -0500

    renamed game template .asm file

--- demos/game_template/compile.sh
+++ demos/game_template/compile.sh
@@ -1,2 +1,2 @@
 mkdir bin
-dasm collect.asm -l"./bin/collect.txt" -f3 -v5 -DNO_ILLEGAL_OPCODES=1 -o"./bin/collect.bin"
+dasm game.asm -l"./bin/game.txt" -f3 -v5 -DNO_ILLEGAL_OPCODES=1 -o"./bin/game.bin"
